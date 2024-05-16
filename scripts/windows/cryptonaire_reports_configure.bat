84f195b37fc45b1d876cc6af92c833b9b17bc7c2
AlexRivas502 <alexrivas502@gmail.com>
2024-05-16 02:14:01 -0700

    Changed script to force reinstall and upgrade

--- scripts/windows/cryptonaire_reports_configure.bat
+++ scripts/windows/cryptonaire_reports_configure.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-py -m pip install -i https://test.pypi.org/simple/  --extra-index-url https://pypi.org/simple cryptonaire-reports
+py -m pip install --upgrade --force-reinstall -i https://test.pypi.org/simple/  --extra-index-url https://pypi.org/simple cryptonaire-reports
 IF NOT EXIST cryptonaire_reports.config curl -o cryptonaire_reports.config https://raw.githubusercontent.com/AlexRivas502/cryptonaire-reports/main/templates/cryptonaire_reports_template.config
