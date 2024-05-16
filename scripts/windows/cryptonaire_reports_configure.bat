d0d9581f35fa5a5d2cd2c6fac25f50e003c3c345
AlexRivas502 <alexrivas502@gmail.com>
2024-05-16 02:10:47 -0700

    Configured script to only create config file if it doesn't exist already

--- scripts/windows/cryptonaire_reports_configure.bat
+++ scripts/windows/cryptonaire_reports_configure.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 py -m pip install -i https://test.pypi.org/simple/  --extra-index-url https://pypi.org/simple cryptonaire-reports
-curl -o cryptonaire_reports_template.config https://raw.githubusercontent.com/AlexRivas502/cryptonaire-reports/main/templates/cryptonaire_reports_template.config
+IF NOT EXIST cryptonaire_reports.config curl -o cryptonaire_reports.config https://raw.githubusercontent.com/AlexRivas502/cryptonaire-reports/main/templates/cryptonaire_reports_template.config
