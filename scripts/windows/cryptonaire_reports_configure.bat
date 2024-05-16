17428537b5bd911814ca555cd5ff3adebdf37fa9
AlexRivas502 <alexrivas502@gmail.com>
2024-05-16 02:04:20 -0700

    Fixed command to always install latest version

--- scripts/windows/cryptonaire_reports_configure.bat
+++ scripts/windows/cryptonaire_reports_configure.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-py -m pip install -i https://test.pypi.org/simple/  --extra-index-url https://pypi.org/simple cryptonaire-reports==0.1.0a2
+py -m pip install -i https://test.pypi.org/simple/  --extra-index-url https://pypi.org/simple cryptonaire-reports
 curl -o cryptonaire_reports_template.config https://raw.githubusercontent.com/AlexRivas502/cryptonaire-reports/main/templates/cryptonaire_reports_template.config
