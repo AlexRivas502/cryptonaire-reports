bf50ef40c72fa07cbba70f26b204100828f87d3e
Alejandro Rivas <alexrivas502@gmail.com>
2024-05-26 21:05:55 -0700

    Alpha Release 5 (#1)
    * Made CoinMarketCap API calls more resilient
    
    * Documented extract_cryptocurrency_map_from_api
    
    * Added numerous debug logs, made all API calls resilient

--- cryptonaire_reports/exchanges/binance.py
+++ cryptonaire_reports/exchanges/binance.py
@@ -2,6 +2,7 @@ import structlog
 
 from typing import Tuple, List
 from binance.spot import Spot
+from binance.api import API
 from cryptonaire_reports.exchanges.exchange import Exchange
 from cryptonaire_reports.utils.singleton import Singleton
 from cryptonaire_reports.utils.symbol_corrector import symbol_corrector
@@ -15,7 +16,12 @@ class Binance(Exchange, metaclass=Singleton):
         super().__init__("Binance")
         if not self.active:
             return
-        self.client = Spot(self._api_key, self._secret_key)
+        self.spot_client = Spot(self._api_key, self._secret_key)
+        self.api = API(
+            api_key=self._api_key,
+            api_secret=self._secret_key,
+            base_url="https://api.binance.com",
+        )
 
     @property
     def name(self) -> str:
@@ -25,51 +31,84 @@ class Binance(Exchange, metaclass=Singleton):
         logger.info(f"[{self.name.upper()}] Extracting balances from Spot account...")
         source_name = "Binance (Spot)"
         spot_balances = []
-        for coin_asset in self.client.account(recvWindow=30000)["balances"]:
-            coin_ticker = symbol_corrector(coin_asset["asset"])
-            if coin_ticker.startswith("LD") and len(coin_ticker) > 4:
-                # This value corresponds to a coin that's stored in Earn - Flexible
-                continue
-            balance = float(coin_asset["free"]) + float(coin_asset["locked"])
-            if not balance > 0:
-                continue
-            spot_balances.append((source_name, coin_ticker, balance))
-        logger.debug(f"[{self.name.upper()}] Spot balances: \n{spot_balances}")
-        return spot_balances
+        try:
+            response = self.spot_client.account(
+                recvWindow=30000, omitZeroBalances="true"
+            )
+            logger.debug(f"[{self.name.upper()}] Full response: {response}")
+            for coin_asset in response["balances"]:
+                coin_ticker = symbol_corrector(coin_asset["asset"])
+                if coin_ticker.startswith("LD") and len(coin_ticker) > 4:
+                    # This value corresponds to a coin that's stored in Earn - Flexible
+                    logger.debug(
+                        f"[{self.name.upper()}] Found coin {coin_ticker}, skipping. "
+                        f"Full info: {coin_asset}"
+                    )
+                    continue
+                balance = float(coin_asset["free"]) + float(coin_asset["locked"])
+                if not balance > 0:
+                    continue
+                spot_balances.append((source_name, coin_ticker, balance))
+            logger.debug(f"[{self.name.upper()}] Spot balances: \n{spot_balances}")
+            return spot_balances
+        except Exception as e:
+            logger.error(
+                f"[{self.name.upper()}] Error while retrieving spot balances from Binance"
+            )
+            logger.debug(f"[{self.name.upper()}] Full exception: {e}")
+            return []
 
     def get_earn_flexible_balances(self) -> List[Tuple[str, str, float]]:
         logger.info(
-            f"[{self.name.upper()}] Extracting balances from Flexible Earn account..."
+            f"[{self.name.upper()}] Extracting balances from flexible earn account..."
         )
         source_name = "Binance (Flexible Earn)"
         earn_balances = []
-        for product in self.client.get_flexible_product_position(recvWindow=30000)[
-            "rows"
-        ]:
-            coin_ticker = symbol_corrector(product["asset"])
-            balance = float(product["totalAmount"])
-            if not balance > 0:
-                continue
-            earn_balances.append((source_name, coin_ticker, balance))
-        logger.debug(f"[{self.name.upper()}] Flexible Earn balances: \n{earn_balances}")
-        return earn_balances
+        try:
+            response = self.spot_client.get_flexible_product_position(recvWindow=30000)
+            logger.debug(f"[{self.name.upper()}] Full response: {response}")
+            for product in response["rows"]:
+                coin_ticker = symbol_corrector(product["asset"])
+                balance = float(product["totalAmount"])
+                if not balance > 0:
+                    continue
+                earn_balances.append((source_name, coin_ticker, balance))
+            logger.debug(
+                f"[{self.name.upper()}] Flexible Earn balances: \n{earn_balances}"
+            )
+            return earn_balances
+        except Exception as e:
+            logger.error(
+                f"[{self.name.upper()}] Error while retrieving flexible balances from Binance"
+            )
+            logger.debug(f"[{self.name.upper()}] Full exception: {e}")
+            return []
 
     def get_earn_locked_balances(self) -> List[Tuple[str, str, float]]:
         logger.info(
-            f"[{self.name.upper()}] Extracting balances from Locked Earn account..."
+            f"[{self.name.upper()}] Extracting balances from locked earn account..."
         )
         source_name = "Binance (Locked Earn)"
         earn_balances = []
-        for product in self.client.get_locked_product_position(recvWindow=30000)[
-            "rows"
-        ]:
-            coin_ticker = symbol_corrector(product["asset"])
-            balance = float(product["amount"])
-            if not balance > 0:
-                continue
-            earn_balances.append((source_name, coin_ticker, balance))
-        logger.debug(f"[{self.name.upper()}] Locked Earn balances: \n{earn_balances}")
-        return earn_balances
+        try:
+            response = self.spot_client.get_locked_product_position(recvWindow=30000)
+            logger.debug(f"[{self.name.upper()}] Full response: {response}")
+            for product in response["rows"]:
+                coin_ticker = symbol_corrector(product["asset"])
+                balance = float(product["amount"])
+                if not balance > 0:
+                    continue
+                earn_balances.append((source_name, coin_ticker, balance))
+            logger.debug(
+                f"[{self.name.upper()}] Locked Earn balances: \n{earn_balances}"
+            )
+            return earn_balances
+        except Exception as e:
+            logger.error(
+                f"[{self.name.upper()}] Error while retrieving locked balances from Binance"
+            )
+            logger.debug(f"[{self.name.upper()}] Full exception: {e}")
+            return []
 
     def get_balances(self) -> List[Tuple[str, str, float]]:
         balances = []
--- cryptonaire_reports/utils/coin_market_cap.py
+++ cryptonaire_reports/utils/coin_market_cap.py
@@ -1,11 +1,11 @@
 import time
 from configparser import ConfigParser
-from requests import Response
 from typing import List, Dict, Set
 
 import structlog
 from coinmarketcapapi import CoinMarketCapAPI
 from coinmarketcapapi import CoinMarketCapAPIError
+from coinmarketcapapi import Response
 from cryptonaire_reports.utils.singleton import Singleton
 
 logger = structlog.get_logger()
@@ -30,6 +30,135 @@ class CoinMarketCap(metaclass=Singleton):
             )
             exit(1)
 
+    def extract_cryptocurrency_map_from_api(self, coin_list: Set[str]) -> List[Dict]:
+        """Calls the cryptocurrency_map endpoint from CoinMarketCap API and retrieves
+        the cryptocurrency map for each of the coins in coin_list. If the call isn't
+        successfull, it retries or fails depending on the error code.
+
+        Args:
+            coin_list (Set[str]): List of coins to look for.
+
+        Returns:
+            List[Dict]: List that contains the cryptocurrency map for all coins
+        """
+        try:
+            coin_market_cap_map_response: Response = self.api.cryptocurrency_map(
+                symbol=",".join(coin_list)
+            )
+            logger.info(
+                f"[CoinMarketCap] Cryptocurrency map information for "
+                f"{','.join(coin_list)} successfully extracted from API"
+            )
+            logger.debug(
+                f"[CoinMarketCap] Full response: {coin_market_cap_map_response}"
+            )
+            return coin_market_cap_map_response.data
+        except CoinMarketCapAPIError as e:
+            error_response: Response = e.rep
+            logger.debug(f"[CoinMarketCap] Full error: {error_response}")
+            if error_response.error_code == 400:
+                # Bad request, one or more coins were not found. Try one by one
+                if len(coin_list) == 1:
+                    # This particular coin was not found, return []
+                    logger.error(
+                        f"[CoinMarketCap] The coin {next(iter(coin_list))} was not "
+                        f"found in CoinMarketCap. All the information will be missing."
+                    )
+                    return []
+                else:
+                    logger.warning(
+                        f"[CoinMarketCap] Failed to fetch cryptocurrency map info for "
+                        "all the coins in one API call. Trying making one call per coin"
+                    )
+                    batch_responses = []
+                    for symbol in coin_list:
+                        res = self.extract_cryptocurrency_map_from_api(
+                            coin_list={symbol}
+                        )
+                        print(res)
+                        batch_responses.extend(res)
+                    return batch_responses
+
+            elif error_response.error_code in [401, 403]:
+                # Forbidden or unauthorized access
+                logger.error(
+                    f"[CoinMarketCap] Failed to retrieve info from API. Access to the "
+                    f"cryptocurrency map is forbidden or unauthorized"
+                )
+                exit(1)
+            elif error_response.error_code in [429, 1008]:
+                # Request limit reached
+                logger.warning(
+                    f"[CoinMarketCap] API limit reached. Waiting 60 seconds to resume..."
+                )
+                time.sleep(61)
+                return self.extract_cryptocurrency_map_from_api(coin_list=coin_list)
+            elif error_response.error_code == 500:
+                # Internal server error
+                logger.error(
+                    f"[CoinMarketCap] There is a problem with the CoinMarketCap API. "
+                    f"Please try again later"
+                )
+                exit(1)
+            else:
+                # Unknown error
+                logger.error(
+                    f"[CoinMarketCap] Unknown error happened. Please create an issue "
+                    f"in the Github project to solve this problem. Include the "
+                    f"following in your request: {error_response}"
+                )
+                exit(1)
+
+    def extract_quotes_latest_from_api(self, id: str) -> List[Dict]:
+        """Calls the cryptocurrency_quotes_latest endpoint from CoinMarketCap API and
+        retrieves the latest price data from a given coin.
+
+        Args:
+            coin_list (Set[str]): CoinMarketCap coin id
+
+        Returns:
+            List[Dict]: Price info for the requested id
+        """
+        try:
+            latest_quote = self.api.cryptocurrency_quotes_latest(id=id).data.get(id)
+            return latest_quote
+        except CoinMarketCapAPIError as e:
+            error_response: Response = e.rep
+            logger.debug(f"[CoinMarketCap] Full error: {error_response}")
+            if error_response.error_code == 400:
+                # Bad request, coin not found.
+                logger.error(f"[CoinMarketCap] Latest quote not found for {id}")
+                return []
+            elif error_response.error_code in [401, 403]:
+                # Forbidden or unauthorized access
+                logger.error(
+                    f"[CoinMarketCap] Failed to retrieve info from API. Access to the "
+                    f"cryptocurrency map is forbidden or unauthorized"
+                )
+                exit(1)
+            elif error_response.error_code in [429, 1008]:
+                # Request limit reached
+                logger.warning(
+                    f"[CoinMarketCap] API limit reached. Waiting 60 seconds to resume..."
+                )
+                time.sleep(61)
+                return self.extract_quotes_latest_from_api(id=id)
+            elif error_response.error_code == 500:
+                # Internal server error
+                logger.error(
+                    f"[CoinMarketCap] There is a problem with the CoinMarketCap API. "
+                    f"Please try again later"
+                )
+                exit(1)
+            else:
+                # Unknown error
+                logger.error(
+                    f"[CoinMarketCap] Unknown error happened. Please create an issue "
+                    f"in the Github project to solve this problem. Include the "
+                    f"following in your request: {error_response}"
+                )
+                exit(1)
+
     def get_coin_info(self, coin_list: Set[str]) -> List[Dict]:
         """Given a list of coins / ticker symbols, extracts additional information
         using the CoinMarketCap API.
@@ -41,19 +170,12 @@ class CoinMarketCap(metaclass=Singleton):
             List[Dict]: Dictionary where the keys are the ticker symbols and the value
                 is a dictionary with all the requested information
         """
-        try:
-            coin_market_cap_map_response: Response = self.api.cryptocurrency_map(
-                symbol=",".join(coin_list)
-            )
-        except:
-            logger.error(
-                f"[CoinMarketCap] Error while retrieving the cryptocurrency map"
-            )
-            return []
+        cryptocurrency_map = self.extract_cryptocurrency_map_from_api(coin_list)
+
         # Results can contain duplicates. We only want to keep the first instance of
         # each symbol
         coin_info = {}
-        for coin_map in coin_market_cap_map_response.data:
+        for coin_map in cryptocurrency_map:
             if coin_map["symbol"].upper() in coin_info:
                 continue
             coin_info[coin_map["symbol"].upper()] = {
@@ -70,29 +192,12 @@ class CoinMarketCap(metaclass=Singleton):
             logger.info(
                 f"[CoinMarketCap] Basic information found for: {', '.join(coin_list)}"
             )
-
-        for symbol, info in coin_info.items():
-            logger.info(f"[CoinMarketCap] Extracting latest price of {symbol}...")
-            try:
-                latest_quote = self.api.cryptocurrency_quotes_latest(
-                    id=info["id"]
-                ).data.get(str(info["id"]))
-            except CoinMarketCapAPIError:
-                logger.warning(
-                    f"[CoinMarketCap] API limit reached. Waiting 60 seconds to reset..."
-                )
-                time.sleep(61)
-                try:
-                    latest_quote = self.api.cryptocurrency_quotes_latest(
-                        id=info["id"]
-                    ).data.get(str(info["id"]))
-                except:
-                    logger.error(f"[CoinMarketCap] Price data not found for: {symbol}")
-                    latest_quote = None
-
+        for symbol, crypto_map in coin_info.items():
+            latest_quote = self.extract_quotes_latest_from_api(id=str(crypto_map["id"]))
             if not latest_quote:
-                logger.warning(f"[CoinMarketCap] Price data not found for: {symbol}")
+                logger.warning(f"[CoinMarketCap] Price data not found for {symbol}")
             else:
+                logger.info(f"[CoinMarketCap] Price data found for {symbol}")
                 coin_info[symbol]["price_usd"] = float(
                     latest_quote.get("quote").get("USD").get("price")
                 )
